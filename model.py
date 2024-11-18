fee6d0f2a49fa7fc783ec0ada124c628c0f3ac68
eddydpan <eddydotpan@gmail.com>
2024-11-18 09:43:00 -0500

    Implement basics of the python-chess library

--- model.py
+++ model.py
@@ -2,8 +2,10 @@
 Initialization of our model for chess. This file stores the data that makes up
 the backend of the game.
 """
-import model
+import chess
 
-board = model.Board()
+board = chess.Board()
 print(board)
-board.legal_moves
+print(type(board.legal_moves))
+legal_moves = [board.san(move) for move in board.legal_moves] # list of legal moves
+print(board)
