4721053ed807bb25d9f89c24efebc61a81a498c3
eddydpan <eddydotpan@gmail.com>
2024-12-04 13:13:49 -0500

    Add eval files

---
+++ .test_eval.py
@@ -0,0 +1,30 @@
+"""
+A module that tests the implementation frmo eval.py
+"""
+
+import chess
+import eval
+
+board = chess.Board()
+legal_moves = [board.san(move) for move in board.legal_moves] # list of legal moves
+
+
+# print(legal_moves)
+print(board)
+
+score = eval.evaluate_board(board)
+print(f"Score is {score}")
+
+board.push_san("e4")
+board.push_san("e5")
+
+score = eval.evaluate_board(board)
+print(f"Score is {score}")
+print(board)
+
+board.push_san("Nf3")
+board.push_san("Nc6")
+
+score = eval.evaluate_board(board)
+print(f"Score is {score}")
+print(board)
